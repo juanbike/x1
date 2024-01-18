a298eb1563dee45a8374cb1f8eec9441a435d77d
juanbike <juancarlos.arteaga1@gmail.com>
2024-01-17 20:44:50 -0400

    agregados codigos de estado http para informar al usuario de algun error del lado del cliente o servidor

--- src/app/componentes/tabla-juntas/tabla-juntas.component.ts
+++ src/app/componentes/tabla-juntas/tabla-juntas.component.ts
@@ -42,7 +42,7 @@ import { NotificationsComponent } from '../notifications/notifications.component
     MatInputModule,
     ModalagregarjuntaComponent,
     ReactiveFormsModule,
-   NotificationsComponent,
+    NotificationsComponent,
   ], //importamos los modulos
   templateUrl: './tabla-juntas.component.html',
   styleUrl: './tabla-juntas.component.css',
@@ -55,8 +55,6 @@ export class TablaJuntasComponent {
     public dialog: MatDialog, //inicializamos el cuadro de dialogo: Eliminar Junta
     private snackBar: MatSnackBar //inicializamos el snackbar para mostrar notificaciones al usuario
   ) {}
-
-
 
   allJuntas: Junta_interface[] = []; //arreglo de juntas
   datosDesdeHijo!: string;
@@ -92,26 +90,21 @@ export class TablaJuntasComponent {
     'Acciones',
   ]; // Columnas de la tabla
 
-
-
-
-/*
+  /*
 ********************************************************************
 Metodo para obtener los datos de la API y mostrarlos en la tabla
 ********************************************************************
 */
 
-
   ngOnInit(): void {
     this.fetchJuntas();
   }
 
-/*
+  /*
 ********************************************************************
   Metodo para obtener los datos de la API y mostrarlos en la tabla
 ********************************************************************
 */
-
 
   private fetchJuntas() {
     // Muestra un indicador de carga mientras se realiza la solicitud
@@ -127,26 +120,50 @@ Metodo para obtener los datos de la API y mostrarlos en la tabla
 
         this.dataSource.paginator = this.paginator; // Asigna el paginador a la fuente de datos
         this.dataSource.sort = this.sort; // Asigna el sort a la fuente de datos
-        this.openSnackBar('Recuperando registros de la base de datos', 'Cerrar');
+        this.openSnackBar(
+          'Recuperando registros de la base de datos',
+          'Cerrar'
+        );
       },
       // Maneja errores en la solicitud HTTP
       (error: HttpErrorResponse) => {
-        this.isLoading = false;
-        // Mejora: Muestra un mensaje de error más amigable
-        console.error('Error al cargar juntas:', error);
-        this.openSnackBar('Ocurrio un error: `{{ error}}` al cargar las juntas', 'Cerrar');
+        // Manejo de errores más detallado
+        let errorMessage = 'Ocurrió un error al cargar las juntas.';
+
+        if (error.status === 0) {
+          errorMessage =
+            'No se pudo conectar al servidor. Verifique su conexión a internet.';
+        } else if (error.status >= 400 && error.status < 500) {
+          // Errores del lado del cliente (por ejemplo, errores de validación)
+          errorMessage =
+            'Error en la solicitud. Verifique los datos ingresados.';
+            if(error.status == 400){
+              errorMessage = 'La solicitud no pudo ser entendida o procesada por el servidor debido a una sintaxis incorrecta o a parámetros inválidos..'
+            } else if (error.status == 401){
+              errorMessage = 'El cliente debe autenticarse para obtener la respuesta solicitada.'
+            } else if (error.status == 403){
+              errorMessage = 'El cliente no tiene los privilegios necesarios para obtener la respuesta solicitada.'
+            } else if (error.status == 404){
+              errorMessage = 'El recurso solicitado no fue encontrado en el servidor'
+            } else if (error.status == 405){
+              errorMessage = 'El servidor no admite la solicitud HTTP solicitada.'
             }
+        } else if (error.status >= 500) {
+          // Errores del lado del servidor
+          errorMessage =
+            'Error interno del servidor. Inténtelo de nuevo más tarde o contacte a soporte técnico.';
+        }
+        // Muestra un mensaje de error utilizando MatSnackBar
+        this.openSnackBar(errorMessage, 'Cerrar');
+      }
     );
   }
-
-
 
   /*
 ********************************************************************
   Metodo para eliminar una junta
 ********************************************************************
 */
-
 
   onDeleteJunta(id: string): void {
     const juntaEncontrada = this.juntas.find(
@@ -156,34 +173,28 @@ Metodo para obtener los datos de la API y mostrarlos en la tabla
     if (juntaEncontrada) {
       this.juntasService.onDeleteJunta(id).subscribe(
         () => {
-        // Operación exitosa
-        console.log('Junta eliminada');
-        this.openSnackBar('Registro eliminado', 'Cerrar');
+          // Operación exitosa
+          console.log('Junta eliminada');
+          this.openSnackBar('Registro eliminado', 'Cerrar');
 
-        this.fetchJuntas(); // Recarga la tabla con los nuevos datos
-      },
-      (error: HttpErrorResponse) => {
-        this.openSnackBar('Ocurrio un error al eliminar una junta', 'Cerrar');
-      }
-    );
+          this.fetchJuntas(); // Recarga la tabla con los nuevos datos
+        },
+        (error: HttpErrorResponse) => {
+          this.openSnackBar('Ocurrio un error al eliminar una junta', 'Cerrar');
+        }
+      );
     }
   }
 
-
-
-/*
+  /*
 ********************************************************************
   Metodo para ocultar el modal
 ********************************************************************
 */
 
-
-
-
   OnAddJunta(): void {
     this.showModalAgregarJuntas = true; // Mostrar el componente modal
   }
-
 
   /*
 *********************************************************************************************************************************************
@@ -217,25 +228,22 @@ Si la fuente de datos tiene paginación habilitada (this.dataSource.paginator), 
     this.showModalAgregarJuntas = false; // Ocultar el componente modal
   }
 
-
-
-/*
+  /*
 ********************************************************************
 configuramos el snackbar Muestra una notificación al usuario.
 ********************************************************************
 */
 
-openSnackBar(message: string, action: string) {
-  const config = new MatSnackBarConfig();
-  config.panelClass = ['blue-snackbar'];
-  config.horizontalPosition = 'end'; // Posición horizontal: 'start' | 'center' | 'end' | 'left' | 'right'
-  config.verticalPosition = 'top'; // Posición vertical: 'top' | 'bottom'
-  config.duration = 3000; // Duración en milisegundos (opcional)
-  this.snackBar.open(message, action, config);
-}
+  openSnackBar(message: string, action: string) {
+    const config = new MatSnackBarConfig();
+    config.panelClass = ['blue-snackbar'];
+    config.horizontalPosition = 'end'; // Posición horizontal: 'start' | 'center' | 'end' | 'left' | 'right'
+    config.verticalPosition = 'top'; // Posición vertical: 'top' | 'bottom'
+    config.duration = 3000; // Duración en milisegundos (opcional)
+    this.snackBar.open(message, action, config);
+  }
 
-
-/*
+  /*
 ******************************************************************************************************************
 este código abre un cuadro de diálogo de confirmación utilizando Angular Material, pasando un mensaje, un texto
 para el botón y un identificador asociado a la junta que se desea eliminar. Este enfoque es útil para encapsular
@@ -243,41 +251,36 @@ la lógica del cuadro de diálogo en un componente separado (BoxDialogComponent)
 ********************************************************************************************************************
 */
 
+  openConfirmDialog(id: string): void {
+    const dialogRef = this.dialog.open(BoxDialogComponent, {
+      //data: { id: id },
+      width: '250px',
+      data: {
+        message: '¿Deseas eliminar esta junta?',
+        buttonText: 'Eliminar',
+        id: id,
+      },
+    });
 
-openConfirmDialog(id: string): void {
-  const dialogRef = this.dialog.open(BoxDialogComponent, {
-    //data: { id: id },
-    width: '250px',
-    data: { message: '¿Deseas eliminar esta junta?', buttonText: 'Eliminar', id: id },
-  });
-
-
-/*
+    /*
 **********************************************************************************************
 Este código proporciona una estructura básica para manejar los resultados después de cerrar
 un diálogo y realizar acciones específicas en este caso eliminar una junta o no.
 ************************************************************************************************
 */
 
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result && result.confirmed) {
+        this.onDeleteJunta(result.id);
 
-  dialogRef.afterClosed().subscribe((result) => {
-    if (result && result.confirmed) {
+        // El usuario hizo clic en "Eliminar"
 
-      this.onDeleteJunta(result.id);
-
-      // El usuario hizo clic en "Eliminar"
-
-      console.log('Registro eliminado');
-    } else {
-      // El usuario hizo clic en "Cancelar" o cerró el cuadro de diálogo
-      console.log('Eliminación cancelada');
-      this.openSnackBar('Eliminación cancelada', 'Cerrar');
-    }
-  });
-}
-
-
-
-
-
+        console.log('Registro eliminado');
+      } else {
+        // El usuario hizo clic en "Cancelar" o cerró el cuadro de diálogo
+        console.log('Eliminación cancelada');
+        this.openSnackBar('Eliminación cancelada', 'Cerrar');
+      }
+    });
+  }
 }
